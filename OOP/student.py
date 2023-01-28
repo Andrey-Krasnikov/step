55a63ec0acfe53d4aa3cb2daaccf01b000b26fd3
Andrey-Krasnikov <a8827n@gmail.com>
2023-01-28 19:28:20 +0200

    initial commit

--- OOP/student.py
+++ OOP/student.py
@@ -30,3 +30,51 @@
 # student2.grow(height=15)
 # print(student2.height)
 
+#СИМУЛЯТОР СТУДЕНТА
+from random import randint
+class Student:
+    def __init__(self, name):
+        self.name=name
+        self.happy=50
+        self.progress=0
+        self.live=True
+
+    def study(self):
+        print("Время учится")
+        self.happy-10
+        self.progress+=5
+    def sleep(self):
+        print("Время сна")
+        self.happy+=5
+    def rest(self):
+        print("Время отдыха")
+        self.happy+=10
+        self.progress-=5
+    def islive(self):
+        if self.happy<=0:
+            print("Депрессия")
+            self.live=False
+        if self.progress>=20:
+            print("Сессия сдана. Есть больше времени дня отдыха")
+            self.happy+=15
+            self.live = False
+        if self.progress<-5:
+            print("Сесстя НЕ СДАНА. Ты отчислен!")
+            self.live = False
+    def day(self):
+        print("Уровень счастья: ", self.happy)
+        print("Успеваемость в учебе: ", self.progress)
+
+    def choice(self,numDay):
+        numDay="День №"+str(numDay)+"из жизни студента"+self.name
+        print(f"{numDay:=^50}")
+        rnd=randint(1,3)
+        if rnd==1: self.study()
+        elif rnd ==2: self.sleep()
+        else: self.rest()
+        self.day()
+        self.islive()
+
+student=Student(name="Андрей")
+for numDay in range(7):
+    student.choice(numDay)
