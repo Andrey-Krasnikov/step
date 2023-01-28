4e35aded8aa4f0a20cee85301675e8afe0341e93
Andrey-Krasnikov <a8827n@gmail.com>
2023-01-28 17:40:19 +0200

    initial commit

--- OOP/student.py
+++ OOP/student.py
@@ -3,12 +3,24 @@ from random import randint as rand
 
 class Student:
     #print("класс Студнееент")
+    col=0 #атрибут класса
     def __init__(self, height=170):
         self.height=height
         #print("Я студент")
         #print(self)
+        Student.col+=1 #глобальная
+    def grow(self, height=5):
+        self.height+=height
 student1=Student() #объект класса
-
+print("Количество студентов: ", student1.col)
 print("Средний рост студента №1: ",student1.height)
+print(student1.col)
+student1.grow(height=5)
+print(student1.height)
+#print(Student.col) #аналогично работает
 student2=Student(height=180)
+print(student2.col)
+print("Количество студентов: ", student2.col)
 print("Средний рост студента №2: ",student2.height)
+student2.grow(height=15)
+print(student2.height)
