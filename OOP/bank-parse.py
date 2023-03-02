13230deffc68421a43021d2584483e6a689cc994
Andrey-Krasnikov <a8827n@gmail.com>
2023-03-02 20:55:23 +0200

    initial commit

--- OOP/bank-parse.py
+++ OOP/bank-parse.py
@@ -1,20 +1,38 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+import tkinter as tk
+
 
 def usd():
-    url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=USD&date="
-    res = requests.get(url)
-    soup = bs(res.text, 'html.parser')
-    usd_rate = soup.find('rate').text
-    print(f"Актуальный курс доллара к гривне: {usd_rate}")
+    try:
+        url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=USD&date="
+        res = requests.get(url)
+        soup = bs(res.text, 'html.parser')
+        usd_rate = soup.find('rate').text
+        number_label.config(text=usd_rate)
+    except Exception as e:
+        print("Error fetching USD rate:", e)
 
 def eur():
-    url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=EUR&date="
-    res = requests.get(url)
-    soup = bs(res.text, 'html.parser')
-    usd_rate = soup.find('rate').text
-    print(f"Актуальный курс евро к гривне: {usd_rate}")
+    try:
+        url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=EUR&date="
+        res = requests.get(url)
+        soup = bs(res.text, 'html.parser')
+        eur_rate = soup.find('rate').text
+        number_label.config(text=eur_rate)
+    except Exception as e:
+        print("Error fetching EUR rate:", e)
 
-usd()
-eur()
+
+window = tk.Tk()
+window.title("Курс валют")
+window.geometry("300x200")
+usd_button = tk.Button(window, text="USD", fg="white", bg="red", padx=10, pady=5, command=usd)
+eur_button = tk.Button(window, text="EUR", fg="white", bg="red", padx=10, pady=5, command=eur)
+usd_button.pack(pady=10)
+eur_button.pack(pady=10)
+number_label = tk.Label(window, text="0", font=("Helvetica", 48), fg="blue")
+number_label.pack(pady=20)
+
+window.mainloop()
